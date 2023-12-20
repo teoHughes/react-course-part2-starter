7587cce89acaf655dba06f8ffcc6ca2fbe08d601
Teo Hughes <th1522@ic.ac.uk>
2023-12-20 13:25:29 +0000

    countReducer

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,10 @@ import "./App.css";
 import PostList from "./react-query/PostList";
 import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
+import Counter from "./state-management/Counter";
 
 function App() {
-  return (
-    <>
-      <TodoForm />
-      <TodoList />
-    </>
-  );
+  return <Counter />;
 }
 
 export default App;

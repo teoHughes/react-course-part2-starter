5ab1ca9e4617df93721222da11712e0a68cdb4fb
Teo Hughes <th1522@ic.ac.uk>
2023-12-20 18:59:21 +0000

    Implement zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import NavBar from "./state-management/NavBar";
 import HomePage from "./state-management/HomePage";
-import AuthProvider from "./state-management/auth/AuthProvider";
 import { TasksProvider } from "./state-management/tasks";
+import Counter from "./state-management/counter/Counter";
 
 function App() {
   return (
-    <AuthProvider>
-      <TasksProvider>
-        <NavBar />
-        <HomePage />
-      </TasksProvider>
-    </AuthProvider>
+    <TasksProvider>
+      <Counter />
+      <NavBar />
+      <HomePage />
+    </TasksProvider>
   );
 }
 
--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -1,13 +1,15 @@
-import { useContext } from "react";
+import { useCallback, useContext } from "react";
 import LoginStatus from "./auth/LoginStatus";
 import TasksContext from "./tasks/tasksContext";
+import useCounterStore from "./counter/store";
 
 const NavBar = () => {
   const { tasks } = useContext(TasksContext);
+  const { counter } = useCounterStore();
 
   return (
     <nav className="navbar d-flex justify-content-between">
-      <span className="badge text-bg-secondary">{tasks.length}</span>
+      <span className="badge text-bg-secondary">{counter}</span>
       <LoginStatus />
     </nav>
   );

86080cf0979972889fe7fef0a9a28a31bc361a09
Teo Hughes <th1522@ic.ac.uk>
2023-12-21 14:23:53 +0000

    Handling errors and styling active link

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -3,11 +3,13 @@ import HomePage from "./HomePage";
 import UserDetail from "./UserDetail";
 import Layout from "./Layout";
 import UsersPage from "./UsersPage";
+import ErrorPage from "./ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {

55c68bb17b4554474c2d187bb9b84026880bc4d9
Teo Hughes <th1522@ic.ac.uk>
2023-12-21 14:05:36 +0000

    Passing data through routes

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import App from "./App";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routing/routes";
